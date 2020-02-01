2af4a694a71b1190ae82b4b683525c8bb54f32e0
sohan <sohankd5419@gmail.com>
2020-02-01 22:50:10 +0530

    local.js which used for serving static file has been modified to resolve the issue of serving the static file based on requsetd url

--- gulp/local.js
+++ gulp/local.js
@@ -15,8 +15,29 @@ function setAppRoot(app_root)
 }
 
 function local(){
+    /*  -------------1ST APPROACH------------
+        If we use the following way to serve our static files, it will search for every file
+        with requested route. For example, if we search for localhost:4711,by default it will 
+        serve the index.html/js. But if we search for other routes like localhost:4711/error (or)
+        localhost:4711/docs/file, it will search for 'error' or 'docs/file' in the provided root
+        path respectively. So to overcome this problem, we will follow second approach.
+    */
+    // app.use( express.static( path.join( this.app_root ) ));  // First Approach
     
-    app.use( express.static( path.join( this.app_root ) ));
+    /* --------------2ND APPROACH-------------
+        In this approach, whenever a user hits a url from browser, the 1st line will be executed
+        which will search for requested content and will be served by static(),if the route matches 
+        ny of the route present in the array of routes otherwise it will pass the request to next
+        router-handler/middleware (here 2nd line is the next router-handler) by invoking next().
+        The 2nd router will serve the index.html from the assigned directory regardless of requested
+        route/content.
+        For example,if user search for localhost:4711/,it will serve the default static file i.e
+        index.html/js,present in the assigned directory. But when user search for localhost:4711/error
+        or localhost:4711/user/doc, it will search and serve them,if found,otherwise it will serve the
+        index.html file.
+    */
+    app.use( ['/','/src','/LocalDist'],express.static( path.join( this.app_root ) ));
+    app.use( '/*',express.static( path.join( this.app_root,'index.html' ) ));
     
     var port = gulp_config.port;
 
