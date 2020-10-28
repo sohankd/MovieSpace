9fc2c16e110b8ee742cedec6248b628091ac241f
sohan <sohankd5419@gmail.com>
2020-10-28 19:26:21 +0530

    pagination module added

---
+++ src/Modules/GlobalViews/JavaScript/Global.Pagination.View.js
@@ -0,0 +1,54 @@
+define('Global.Pagination.View'
+,   [
+        'text!src/Modules/GlobalViews/Template/global_pagination.hbs'
+    ,   'Marionette'
+    ]
+,   function
+    (
+        global_pagination_tpl
+    ,   Marionette
+    )
+{
+    'use strict';
+
+    return Marionette.View.extend({
+        className: 'global-pagination-container'
+
+    ,   template: global_pagination_tpl
+        
+    ,   templateContext: function() {
+            var current_page = this.getOption('currentPage') || 0
+            ,   total_pages = this.getOption('totalPages') || 0
+            ,   total_results = this.getOption('totalResults') || 0
+            ,   use_load_btn = this.getOption('useLoadButton') || false
+            ,   showEndPageLinks = this.getOption('showEndPageLinks') || true
+            ,   pages = []
+            ,   steps = 2;
+
+            if(!use_load_btn) {
+                var page_start_index = current_page > steps ? current_page - steps : 1
+                ,   pages_remain = total_pages - current_page
+                ,   page_last_index = pages_remain > steps ? current_page + steps : total_pages;
+
+                for(var i = page_start_index; i <= page_last_index; i++){
+                    pages.push({
+                        page: i
+                    ,   text:i
+                    ,   isActive: current_page == i
+                    });
+                }
+            }
+            
+            return {
+                current_page: current_page
+            ,   total_pages: total_pages
+            ,   total_results: total_results
+            ,   use_load_btn: use_load_btn
+            ,   pages: pages
+            ,   showEndPageLinks: showEndPageLinks
+            ,   disableEndPageLink: current_page == total_pages
+            ,   showLoadMoreButton: current_page != total_pages
+            }
+        }
+    });    
+});
