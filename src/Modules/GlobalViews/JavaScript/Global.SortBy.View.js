2b1a465c72bd7ed7a5707bbb2dbd1d6095fd9a53
sohan <sohankd5419@gmail.com>
2021-06-16 21:53:06 +0530

    SortBy View template updated for dark theme

--- src/Modules/GlobalViews/JavaScript/Global.SortBy.View.js
+++ src/Modules/GlobalViews/JavaScript/Global.SortBy.View.js
@@ -34,7 +34,7 @@ define('Global.SortBy.View'
         ]
 
     ,   events: {
-            'change [data-action="updateSorting"]': 'sortByHandler'
+            'click [data-action="updateSorting"]': 'sortByHandler'
         ,   'change [data-action="changeSortOrder"]': 'sortByHandler'
         }
 
@@ -51,20 +51,24 @@ define('Global.SortBy.View'
 
     ,   sortByHandler: _.debounce(function(e){
             var sortById
-            ,   url = new UrlParse(location.href);
+            ,   url = new UrlParse(location.href)
+            ,   shouldRedirect = true;
 
-            if(e.currentTarget.nodeName == "SELECT"){
-                sortById = this.$(e.currentTarget).val();
-                _.each(this.sortOptions, sortOption => { sortOption['isActive'] = (sortById == sortOption['id']) });
+            if(e.currentTarget.nodeName == "LI"){
+                sortById = this.$(e.currentTarget).data('value');
+                _.each(this.sortOptions, sortOption => {
+                    sortById == sortOption['id'] && (sortOption['isActive'] ? (shouldRedirect = false) : (sortOption['isActive'] = true));
+                });
             }
             else if(e.currentTarget.nodeName == "INPUT"){
                 this.order = this.$(e.currentTarget).is(':checked') ? 'desc' : 'asc';
                 sortById = _.findWhere(this.sortOptions, {isActive: true}).id;
             }
-
-            url.removeParams('page', true);
-            url = url.setParam('sort_by', sortById + '.' + this.order);
-            Backbone.history.navigate(url.getFragment(), true);
+            if(shouldRedirect){
+                url.removeParams('page', true);
+                url = url.setParam('sort_by', sortById + '.' + this.order);
+                Backbone.history.navigate(url.getFragment(), true);
+            }
             
         }, 500)
 
