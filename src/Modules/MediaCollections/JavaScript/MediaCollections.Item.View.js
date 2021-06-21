f8360956af86e187fce64347f29d5a0565a4686a
sohan <sohankd5419@gmail.com>
2021-06-21 17:15:21 +0530

    rating ui on media cell updated

--- src/Modules/MediaCollections/JavaScript/MediaCollections.Item.View.js
+++ src/Modules/MediaCollections/JavaScript/MediaCollections.Item.View.js
@@ -1,14 +1,14 @@
 define('MediaCollections.Item.View'
 ,   [
         'text!src/Modules/MediaCollections/Template/media_collections_item.hbs'
-    ,   'Media.Rating.View'
     ,   'Marionette'
+    ,   'Utils'
     ]
 ,   function
     (
         media_collections_item_tpl
-    ,   MediaRatingView
     ,   Marionette
+    ,   Utils
     )
 {
     'use strict';
@@ -18,28 +18,11 @@ define('MediaCollections.Item.View'
         
     ,   template: media_collections_item_tpl
 
-    ,   regions: {
-            'Media.Rating': '[data-view="Media.Rating"]'
-        }
-
-    ,   childViews: {
-            'Media.Rating': function() {
-                return new MediaRatingView({
-                    application: this.getOption('application')
-                ,   maxRating: 10
-                ,   rating: this.model.get('vote_average')
-                })
-            }
-        }
-
     ,   templateContext: function() {
-            var popularity = this.model.get('popularity')
-            ,   popularity_formatted = popularity >= 100000 ? parseFloat(popularity/1000000).toFixed(2)+'M' : parseFloat(popularity/1000).toFixed(2)+'K';
             return {
                 media_type: this.getOption('media_type')
-            ,   popularity_formatted: popularity_formatted
-            ,   media_type: this.getOption('media_type')
-            }
+            ,   poster_size: Utils.getDeviceType() == "Mobile" ? 'w185' : 'w300'
+            };
         }
 
     });
